86d00c33907190b60f6701fc60a58cf2f993cd63
hersheysxz <rachelregacho645@gmail.com>
2026-07-02 05:24:18 +0800

    Point frontend contact form to Render backend

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,7 +1,8 @@
 (function () {
   const localHosts = new Set(['', 'localhost', '127.0.0.1']);
+  const productionApiBase = 'https://personalprofile-4wet.onrender.com';
 
   window.PORTFOLIO_CONFIG = {
-    API_BASE_URL: localHosts.has(window.location.hostname) ? 'http://localhost:5000' : ''
+    API_BASE_URL: localHosts.has(window.location.hostname) ? 'http://localhost:5000' : productionApiBase
   };
 })();
